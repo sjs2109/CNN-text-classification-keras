269157138e05ee24a1e4fbb0ec2ee485a425e0c9
sjs2109 <sjs2109@gmail.com>
2018-05-26 21:17:44 +0900

    merge tensorflow modification

--- data_helpers.py
+++ data_helpers.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 import itertools
 from collections import Counter
+import pandas as pd
 
 
 def clean_str(string):
@@ -25,16 +26,18 @@ def clean_str(string):
     return string.strip().lower()
 
 
-def load_data_and_labels():
+def load_data_and_labels(data_file):
     """
     Loads polarity data from files, splits the data into words and generates labels.
     Returns split sentences and labels.
     """
     # Load data from files
-    positive_examples = list(open("./data/rt-polarity.pos", "r", encoding='latin-1').readlines())
-    positive_examples = [s.strip() for s in positive_examples]
-    negative_examples = list(open("./data/rt-polarity.neg", "r", encoding='latin-1').readlines())
-    negative_examples = [s.strip() for s in negative_examples]
+
+    dfRaw = pd.read_csv(data_file)
+    dfRec = dfRaw[['Review Text', 'Recommended IND']].dropna()
+    positive_examples = dfRec[dfRec['Recommended IND'] == 1]['Review Text'].tolist()
+    negative_examples = dfRec[dfRec['Recommended IND'] == 0]['Review Text'].tolist()
+
     # Split by words
     x_text = positive_examples + negative_examples
     x_text = [clean_str(sent) for sent in x_text]
@@ -91,7 +94,7 @@ def load_data():
     Returns input vectors, labels, vocabulary, and inverse vocabulary.
     """
     # Load and preprocess data
-    sentences, labels = load_data_and_labels()
+    sentences, labels = load_data_and_labels("./data/Womens Clothing E-Commerce Reviews_filtered.csv")
     sentences_padded = pad_sentences(sentences)
     vocabulary, vocabulary_inv = build_vocab(sentences_padded)
     x, y = build_input_data(sentences_padded, labels, vocabulary)
