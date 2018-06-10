d31c5dd7cd6760bbb93c0707362a467903df5fe6
Ubuntu <ubuntu@ip-172-31-7-255.us-west-2.compute.internal>
2018-06-10 15:20:30 +0000

    add early stop

--- model_rnn.py
+++ model_rnn.py
@@ -17,7 +17,7 @@ cvscores = []
 
 max_features = 20000
 text_max_words = 200
-
+cell = 128
 # 1. 데이터셋 생성하기
 
 # 훈련셋과 시험셋 불러오기
@@ -40,21 +40,25 @@ x = sequence.pad_sequences(x, maxlen=text_max_words)
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
 cvscores = []
 for train, test in kfold.split(x, y):
+    x_val = x[train][:int((x[train].shape[0]) * 0.2)] 
+    x_train  = x[train][int((x[train].shape[0]) * 0.2):] 
+    y_val = y[train][:int((y[train].shape[0]) * 0.2)] 
+    y_train = y[train][int((y[train].shape[0]) * 0.2):]
+ 
     # 2. 모델 구성하기
     model = Sequential()
-    model.add(Embedding(len(vocabulary), 1024))
-    model.add(LSTM(1024))
+    model.add(Embedding(len(vocabulary), cell))
+    model.add(LSTM(cell))
     model.add(Dense(1, activation='sigmoid'))
 
     # 3. 모델 학습과정 설정하기
-    checkpoint = ModelCheckpoint('weights.{epoch:03d}-{acc:.4f}.hdf5', monitor='acc', verbose=1, save_best_only=True, mode='auto')
-    early_stopping = EarlyStopping(monitor='val_acc', min_delta=0, patience=5, verbose=1, mode='auto')
+    checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto')
     # 4. 모델 학습시키기
-    hist = model.fit(x[train], y[train], epochs=20, batch_size=32,callbacks=[checkpoint, early_stopping])
+    hist = model.fit(x_train, y_train, epochs=20, batch_size=32,validation_data=(x_val, y_val),verbose=1,callbacks=[early_stopping, checkpoint])
 
-    scores = model.evaluate(x[test], y[test], verbose=0)
+    scores = model.evaluate(x[test], y[test], verbose=1)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
     cvscores.append(scores[1] * 100)
 
